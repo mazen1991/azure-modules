7ee7115ec760c14b2bd89dd97d5e9e5c164654c5
mazen-bassiouny <mazen.atef2012@gmail.com>
2020-09-30 09:27:25 +0200

    first version storage account module

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "saremotestate"{
+  name                     = var.saname
+  resource_group_name      = var.rgname
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
---
+++ output.tf
@@ -0,0 +1,4 @@
+output "primary_key" {
+    value = azurerm_storage_account.saremotestate.primary_access_key
+    
+}
